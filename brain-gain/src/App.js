d59b40809a629b41918a0a0cc07e1773a1b44b43
Moses <Moseschizaram8@gmail.com>
2024-04-26 00:54:52 +0100

    props, jsx and component lesson taken

--- brain-gain/src/App.js
+++ brain-gain/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// function App() {
+//   // const name = "Moses Sunday";
+//   return (
+//     <div className="App">
+//       <p>This is a component</p>
+//       <GetName />
+//       <p>This is a prop</p>
+//       <User name="Moses" age={25} email="moseschizaram8@gmail.com" />
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Job salary={90000} position="Senior SDE" company="Amazon" />
+      <Job salary={12000} position="Junior SDE" company="Google" />
+      <Job salary={90000} position="Project Manager" company="Netflix" />
     </div>
   );
 }
 
+function Job(props) {
+  return (
+    <div className="User">
+      <h4>{props.salary}</h4>
+      <h4>{props.position}</h4>
+      <h4>{props.company}</h4>
+    </div>
+  );
+}
+function GetName() {
+  return (
+    <div>
+      <h1>Pedro</h1>
+      <h1>21</h1>
+      <h1>moseschizaram8@gamil.com</h1>
+    </div>
+  );
+}
+
+const User = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h2>{props.age}</h2>
+      <h2>{props.email}</h2>
+    </div>
+  );
+};
 export default App;
---
+++ glossary.md
@@ -0,0 +1,35 @@
+# JSX =
+
+Combination of html and js
+e.g `return <h1>{name}</h1>;`
+
+# Components
+
+Javascript function that returns a
+jsx object.
+
+<!-- This is a component -->
+<!-- Components returns a ui -->
+
+`function GetNameComponent=()=>{
+    return <h1>{name}</h1>;
+}`
+
+<!-- while this is a javascript function -->
+
+`function GetName=()=>{
+    return "Pedro";
+}`
+
+# props
+
+These are javascript objects that exist as a function argument.
+`
+const User= (props)=>{
+return {
+
+<div>props.name</div>
+ <div>props.agename</div>
+ <div>props.enail</div>
+}}
+`
