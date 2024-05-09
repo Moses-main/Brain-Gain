e4136c0e349741b6681b8e5d490cdaa5fc67be21
Moses <Moseschizaram8@gmail.com>
2024-05-09 12:23:15 +0100

    Learnt use effect: Which is used to render some UI based on some combination of certain features

--- brain-gain/src/App.js
+++ brain-gain/src/App.js
@@ -1,85 +1,20 @@
 import "./App.css";
 import { useState } from "react";
-import { Task } from "./Task";
+import { Text } from "./Text";
 
 function App() {
-  // "DO HOMEWORKS ";
-  // "WASH DISHES";
-  //
-  const [todoList, setTodoList] = useState([]);
-
-  // State to represent the input value
-  const [newTask, setNewTask] = useState("");
-
-  // function for getting the value of the
-  // input value from the input element
-  const handleChange = (event) => {
-    setNewTask(event.target.value);
-  };
-
-  // function for adding new task to the list of
-  // array
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false,
-    };
-    // adding the newtask to the old todolist using a spread operator
-    // let newTodoList = [...todoList, newTask];
-    // setTodoList(newTodoList);
-
-    setTodoList([...todoList, task]);
-  };
-
-  // The function to  delete task from the list
-  const deleteTask = (id) => {
-    const newTodoList = todoList.filter((task) => {
-      return task.id !== id;
-      // This is equivalent to the code below
-      // if (task === taskName) {
-      //   return false;
-      // } else {
-      //   return true;
-      // }
-    });
-
-    setTodoList(newTodoList);
-  };
-
-  // This is for updating the tasks list
-  const completeTask = (id) => {
-    todoList.map((task) => {
-      if (task.id === id) {
-        return { ...task, completed: true };
-      } else {
-        return task;
-      }
-    });
-  };
-
+  const [showText, setShowText] = useState(false);
   return (
     <div className="App">
-      <h1>Todo List</h1>
-      <div className="addTask">
-        <input onChange={handleChange} />
-        <button type="submit" onClick={addTask}>
-          Add Task
-        </button>
-      </div>
-      <div className="list">
-        {todoList.map((task) => {
-          return (
-            <Task
-              taskName={task.taskName}
-              id={task.id}
-              completed={task.completed}
-              deleteTask={deleteTask}
-              completeTask={completeTask}
-            />
-          );
-        })}
-      </div>
+      <button
+        onClick={() => {
+          setShowText(!showText);
+        }}
+      >
+        ShowText
+      </button>
+
+      {showText && <Text />}
     </div>
   );
 }
